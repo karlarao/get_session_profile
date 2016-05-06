9e7fc81c00bc45cf5b4d891db505b777689088d2
karlarao <karlarao@gmail.com>
2016-05-05 21:52:26 -0400

    sort | uniq trace files

--- get_session_summary.sh
+++ get_session_summary.sh
@@ -1,6 +1,6 @@
 # usage: sh get_session_summary.sh <filter>
 filter=$1
-cat get_session_profile_shortstack*$filter*log | grep "trc" > get_session_profile_summary_$filter.log
+cat get_session_profile_shortstack*$filter*log | grep "trc" | sort | uniq > get_session_profile_summary_$filter.log
 cat get_session_profile_shortstack*$filter*log | grep opi | sed -e $'s/<-/\\\n/g' | sort -r | uniq -c | sort -rnk1 >> get_session_profile_summary_$filter.log
 cat get_session_profile*$filter*log | grep "achains" | sort -rnk1 >> get_session_profile_summary_$filter.log
 cat get_session_profile*$filter*log | grep BUFG | sort -n -k9 >> get_session_profile_summary_$filter.log
